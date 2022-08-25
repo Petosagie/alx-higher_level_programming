c6df36a55d6013c2705028e16fc454417a042b98
petosagie <petosagie@gmail.com>
2022-08-25 11:28:25 -0700

    Python code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-def main():
     a = 1
     b = 2
-    c = add(a, b)
-    print("{} + {} = {}".format(a, b, c))
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
