8d7490baa34cc917eb5a7e2fa2d3f2e48af34774
petosagie <petosagie@gmail.com>
2022-08-25 11:38:03 -0700

    Python code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from calculator_1 import add, sub, mul, div
-
 
 def main():
     a = 10
@@ -9,7 +9,3 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
-if __name__ == "__main__":
-    main()
-
