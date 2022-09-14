a6a3ef80cabb2a374dfa3fd08255afaed87e40ca
petosagie <petosagie@gmail.com>
2022-09-13 18:27:16 -0700

    python codes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Module 0-sqaure
+Defines a sqaure
+"""
+
+
 class Square:
-    """Class Square that defines a square.
-    """
+    """ An empty square template """
     pass
