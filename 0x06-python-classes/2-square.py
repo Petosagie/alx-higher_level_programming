cfe329699fd965d4b97d1f06fe6b4857bbb0f2bf
petosagie <petosagie@gmail.com>
2022-09-13 18:33:48 -0700

    python codes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 2-square
+Defines a square based on module 1-square
+"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Represents blueprint for a square"""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
         """
-        if not isinstance(size, int):
+        Initialize the sqaure.
+        Args:
+            size (int): length of the square's size.
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
