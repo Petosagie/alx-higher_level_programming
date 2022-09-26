39c86d3e81d5ffd099f68758d05c1fe0fa2d821f
petosagie <petosagie@gmail.com>
2022-09-26 09:58:04 -0700

    codes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 1-my_list.py
+that prints the list,
+but sorted (ascending sort)
+"""
 class MyList(list):
     """ Class that inherits the attributes references of class list
     Args:
