55ed56ffb2413ded7fa874d90f64e73574ce95b0
petosagie <petosagie@gmail.com>
2022-09-27 20:55:20 -0700

    codes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-""" Module to print status code """
+"""
+    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    if a status code doesn’t appear, don’t print anything for this status code
+    format: <status code>: <number>
+    status codes should be printed in ascending order
+"""
+
 import sys
 
 
