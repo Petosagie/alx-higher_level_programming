d9f4c495f643c5012de39ae8d845ae5b806e7f41
petosagie <petosagie@gmail.com>
2022-09-27 20:48:52 -0700

    codes

--- 0x0B-python-input_output/12-main.py
+++ 0x0B-python-input_output/12-main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+This project is for pascal trainagle
+that returns a list of lists of integers representing 
+the Pascal’s triangle of n
+"""
+
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
-
 def print_triangle(triangle):
+     """print triangle function"""
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
