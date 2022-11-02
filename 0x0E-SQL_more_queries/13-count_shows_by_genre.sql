87530ad79fcf15faa56a1b0929f423509d8cb0ec
petosagie <petosagie@gmail.com>
2022-11-02 06:41:01 -0700

    more SQL codes

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Lists all genres and shows linked to each genre
+SELECT tv_genres.anme AS genre, COUNT(*) AS 'number_of_shows' FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre ORDER BY number_of_shows DESC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- Lists all cities in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
