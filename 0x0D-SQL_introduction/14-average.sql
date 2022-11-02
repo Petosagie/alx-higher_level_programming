c3a74cc31afa9c87258612ac53e6a9a1cc3f793e
petosagie <petosagie@gmail.com>
2022-11-01 18:21:22 -0700

    SQL codes

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+nano 14-average.sql
+-- Script that computes the score average of all records in a table
+-- Query to computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
+
+
